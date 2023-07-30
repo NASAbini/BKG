e3ef628ddbd7916f6a9800babec08c9e397040dd
NASAbini <139511781+NASAbini@users.noreply.github.com>
2023-07-29 21:57:02 -0700

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,64 @@
+print("==================================================")
+print("Welcome to BKG custom car dealer ship request form")
+print("==================================================")
+
+#ask the user selection of make and model
+print("1-choose your selection of make and model. ")
+print("a-camryToyota")
+print("b-corolahybrid")
+print("c-hghlander")
+makeAndmodel=input("enter your choice of letter: ")
+print(f"1-choice of model, {makeAndmodel} ")
+print("")
+
+# ask the user about apgreading to sunroof
+print("2-would you like to upgrade your car to sunroof? yes or no")
+roofType=input("enter your answer: ")
+print(f"2-upgread roof, {roofType} ")
+print("")
+
+# ask the user for colour choice
+print("3-what is your choice of colour?: ")
+colourType=input("enter your colour selection: ")
+print(f"3-colour type,{colourType} ")
+print("")
+
+# ask the user about apgreading the seat
+print("4-would you like to apgread your car seat to leather seat?yes or no")
+seatType=input("enter your answer: ")
+print(f"4-apgread car seat,{seatType} ")
+print("")
+
+#ask the user choise of engine type
+print("5-what is your choice of engine selection? ")
+print("a-V6")
+print("b-V4")
+print("c-electric engine")
+engineType=input("enter your letter of choice: ")
+print(f"5-engine selection,{engineType} ")
+print("")
+
+# ask the user for apgreading the sound system
+print("6-would you like to apgread the sound system to extra surround delux? yes or no")
+soundSystem=input("enter your answer: ")
+print(f"6-apgread sound system, {soundSystem}")
+print("")
+
+print("thank you for coming!")
+print("")
+
+#display the summery
+
+print("summery")
+
+print(f"1-choice of model,{makeAndmodel}")
+print("")
+print(f"2-upgread roof,{roofType}")
+print("")
+print(f"3-colour type,{colourType}")
+print("")
+print(f"4-apgread car seat,{seatType}")
+print("")
+print(f"5-engine selection,{engineType} ")
+print("")
+print(f"6-apgread sound system, {soundSystem}")
